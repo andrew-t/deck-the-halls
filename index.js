abdffb49a9a42a95b2b904159e2193c35d54caf0
Andrew Taylor <mac@andrewt.net>
2016-12-10 10:31:22 +0000

    check home feed also

--- index.js
+++ index.js
@@ -5,42 +5,47 @@ const Twit = require('twit'),
 			.map(w => w.replace(/[^A-Z]/ig, '')));
 
 twitter.stream('statuses/sample')
-	.on('tweet', tweet => {
-		const words = tweet.text
-			.replace(/\b[\S]+:\/\/\S+\b/g, '')
-			.replace(/\B[@#]\S+\b/g, '')
-			.trim()
-			.replace(/[\s."“”‘;,:–\/\\—?!@~*&£$(){}\[\]]+/g, ' ')
-			.replace(/[’'-`]/g, '')
-			.split(' ')
-			.filter(word => 
-				word != 'RT' &&
-				word[0] != '@' &&
-				/[a-z]/i.test(word))
-			// .filter(word =>
-			// 	word != 'RT' &&
-			// 	/^[a-z\-']+$/i.test(word))
-			.map(word => word.toUpperCase());
+	.on('tweet', process);
 
-		const url = `http://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`;
+twitter.stream('user', { with: 'followings' })
+	.on('tweet', process);
 
-		if (words.length == 2) {
-			console.log(words, url);
-		}
+function process(tweet) {
+	const words = tweet.text
+		.replace(/\b[\S]+:\/\/\S+\b/g, '')
+		.replace(/\B[@#]\S+\b/g, '')
+		.trim()
+		.replace(/[\s."“”‘;,:–\/\\—?!@~*&£$(){}\[\]]+/g, ' ')
+		.replace(/[’'-`]/g, '')
+		.split(' ')
+		.filter(word => 
+			word != 'RT' &&
+			word[0] != '@' &&
+			/[a-z]/i.test(word))
+		// .filter(word =>
+		// 	word != 'RT' &&
+		// 	/^[a-z\-']+$/i.test(word))
+		.map(word => word.toUpperCase());
 
-		if (words.length == 2 &&
-			words[1] != 'CUMBERBATCH' &&
-			words[1] != words[0] &&
-			slapdeback.has(words[0]) &&
-			slapdeback.has(words[1])) {
-			console.log(words,
-				url,
-				'I loved him in Sherlock.');
-			twitter.post('statuses/update', { status: `@{tweet.user.screen_name} I loved him in Sherlock.`,
-				in_reply_to_status_id: tweet.id_str
-			}, (err, data, response) => {
-				console.log('DONE TWEETING',
-					err, data, response);
-			});
-		}
-	});
+	const url = `http://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`;
+
+	if (words.length == 2) {
+		console.log(words, url);
+	}
+
+	if (words.length == 2 &&
+		words[1] != 'CUMBERBATCH' &&
+		words[1] != words[0] &&
+		slapdeback.has(words[0]) &&
+		slapdeback.has(words[1])) {
+		console.log(words,
+			url,
+			'I loved him in Sherlock.');
+		twitter.post('statuses/update', { status: `@{tweet.user.screen_name} I loved him in Sherlock.`,
+			in_reply_to_status_id: tweet.id_str
+		}, (err, data, response) => {
+			console.log('DONE TWEETING',
+				err, data, response);
+		});
+	}
+}
